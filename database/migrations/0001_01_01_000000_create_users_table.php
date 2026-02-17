75ef14d3c4543dee77e88a3582566e803428b321
Laurea Gregori <gregoridelrio@gmail.com>
2026-02-17 18:14:07 +0100

    added migrations and models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('skill_level')->nullable();
+            $table->string('favourite_position')->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
