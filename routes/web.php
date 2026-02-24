3d4e3f16e7f2d7c9e1bb91a59fd3bc7b13627882
Laurea Gregori <gregoridelrio@gmail.com>
2026-02-24 11:10:28 +0100

    add: registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MatchController;
+use App\Http\Controllers\RegistrationController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/matches/{match}/edit', [MatchController::class, 'edit'])->name('matches.edit');
     Route::put('/matches/{match}', [MatchController::class, 'update'])->name('matches.update');
     Route::delete('/matches/{match}', [MatchController::class, 'destroy'])->name('matches.destroy');
+
+    Route::post('/matches/{match}/join', [RegistrationController::class, 'store'])->name('matches.join');
+    Route::delete('/matches/{match}/leave', [RegistrationController::class, 'destroy'])->name('matches.leave');
 });
 
 Route::get('/matches/{match}', [MatchController::class, 'show'])->name('matches.show');
